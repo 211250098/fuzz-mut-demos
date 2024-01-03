5ea1e0bf9f48d7b18110482d6ac4d2a89656cd73
huzzsea <huzzsea@foxmail.com>
2024-01-03 16:42:48 +0800

    解决跨域问题

--- mutest-demo/src/main/java/edu/nju/mutest/http/Pojo/Variation.java
+++ mutest-demo/src/main/java/edu/nju/mutest/http/Pojo/Variation.java
@@ -1,15 +1,15 @@
-package edu.nju.mutest.http.Pojo;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@Component
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Variation {
-
-
-}
+//package edu.nju.mutest.http.Pojo;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Variation {
+//
+//
+//}
